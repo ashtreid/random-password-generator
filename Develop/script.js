5e2070b149e6cd5e959da9bec84c12951b833271
Ash Reid <ashley.reid013@gmail.com>
2023-03-11 13:30:34 -0700

    adds length guards

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,21 @@ const abcLow = 'abcdefghijklmnopqrstuvwxyz';
 const numbers = '0123456789';
 const specialChars = '!"#$%&()*+=,-_./:;<>?@[]^`{}|~';
 
+function generatePassword() {
+    // Asks for the desired length with an input prompt
+  var askPassLength = window.prompt("How long would you like your password? Choose a number between 8 - 128.")
+
+  // Checks to see if the input is between 8 and 128. If not, it raises an alert on the window without returning the invalid input.
+  // Also, if the user clicks, cancel, it will end the prompt. I learned this from the recent student mini project solutions.
+  if (askPassLength < 8 || askPassLength > 128) {
+    alert("Invalid input.");
+    return "";
+  } else if (!askPassLength) {
+    return;
+  }
+
+};
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
